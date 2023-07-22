0bc752e351ad3ff17d2309aa48479cc175f19db7
TaneaT <trifon.tanea03@gmail.com>
2023-07-22 17:23:55 +0300

    Created a branch feature-a and changed the code in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Learning how to use Git");
+        System.out.println("Java 8");
     }
 }
